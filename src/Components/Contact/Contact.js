26cbd0ea390b798233dbcf2d1751a507d9c61bcb
Jake Alexander <imjakeal123@gmail.com>
2021-11-11 00:18:57 -0700

    desktop and mobile design done

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import "./Contact.css";
+import { Link } from "react-router-dom";
+import logo from "../../images/mwi-logo.png";
 
 function Contact() {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className='contact-container'>
+            <section className='contact-left-side'>
+            <img className='contact-logo' src={logo} alt='Midwestern Logo'/>
+            <div className='contact-left-side-content'>
+            <h1 className="contact-left-side-header">
+          <span className="contact-gold-underline">Heading</span> One
+        </h1>
+            <p className='contact-left-side-para'>
+                {`            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do dos eiusmod tempor incididunt ut labore et trace dolore magna aliqua. 
+                
+                Proin sagittis nisl rhoncus mattis rhoncus. At augue eget arcu dictum varius duis at consectetur lorem.`}
+            </p>
+            </div>
+            </section>
+
+            <section className='contact-right-side'>
+            <Link className='contact-page-link contact-link' to="/">home</Link>
+            <div className='contact-right-side-content'>
+            <h1 className='contact-right-side-header'>Heading Two</h1>
+            <form className='contact-form'>
+                <input className='contact-input' placeholder='First Name'></input>
+                <input className='contact-input' placeholder='Last Name'></input>
+                <input className='contact-input' placeholder='Title'></input>
+                <input className='contact-input' type='email' required placeholder='Email'></input>
+                <textarea className='contact-textarea' placeholder='Message'></textarea>
+            </form>
+            <button className="contact-submit learn-more">Submit</button>
+            </div>
+            </section>
         </div>
     )
 }
--- src/Components/Home Page/Home.js
+++ src/Components/Home Page/Home.js
@@ -11,22 +11,25 @@ function Home() {
     <div className="home-container">
       <div className="home-content-container">
         <header className="header-container">
-          <img className='home-logo' src={logo} />
+          <img className='home-logo' src={logo} alt='Midwestern Logo'/>
           <Link className='contact-link' to="/contact">contact</Link>
         </header>
         <div className="section-container">
           <section className="homepage-box-1 homepage-box">
-            <img className='box-img' src={talkie} />
+            <img className='box-img' src={talkie} alt='Midwestern Logo' />
             <h1 className="homepage-box-heading">Heading Two</h1>
-            <p className='homepage-box-para'>
+            <p className='para1 homepage-box-para'>
               Integer accumsan molestie nisl, id faucibus urna accumsan quis.
               Proin vulputate, mauris semper maximus.
+            </p>
+            <p className='para1-media-query homepage-box-para'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
             </p>
             <button className="learn-more">Learn More</button>
           </section>
 
           <section className="homepage-box-2 homepage-box">
-            <img className='rabbit box-img' src={rabbit} />
+            <img className='rabbit box-img' src={rabbit} alt='Midwestern Logo'/>
             <h1 className="rabbit-heading homepage-box-heading">Heading Two</h1>
             <p className='rabbit-p homepage-box-para'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
@@ -35,7 +38,7 @@ function Home() {
           </section>
 
           <section className="homepage-box-1 homepage-box">
-            <img className='box-img' src={shield} />
+            <img className='box-img' src={shield} alt='Midwestern Logo'/>
             <h1 className="homepage-box-heading">Heading Two</h1>
             <p className='homepage-box-para'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
@@ -48,10 +51,16 @@ function Home() {
         <h1 className="homepage-bottom-header">
           <span className="homepage-gold-underline">Heading</span> One
         </h1>
-        <p className='homepage-bottom-text'>
+        <p className='bottom1 homepage-bottom-text'>
           Remove the duplicates in 2 Javascript objects and output the list of
           distinct names in an unordered list when <span style={{color: '#debf79'}}>this link</span> is clicked. If the
           operation has been completed already notify the user that this has
+          already been done.
+        </p>
+        <p className='home-bottom-media homepage-bottom-text'>
+          Remove the duplicates in 2 Javascript objects and output the list of
+          distinct names in an unordered list when <span style={{color: '#debf79'}}>this link</span> is clicked. If the operation has been{'\n'}{'\n'}
+          completed already notify the user that this has
           already been done.
         </p>
 
